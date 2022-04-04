6b220501fdeac858e4216505c057ac829829bb58
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-04-04 14:54:34 +0300

    added css

--- src/AnimalCard.js
+++ src/AnimalCard.js
@@ -4,17 +4,18 @@ const AnimalCard = (props) => {
   return (
     <div className="card">
       <img
+      className="animalImage"
         src={`https://source.unsplash.com/500x400/?${props.name}`}
         alt={props.name}
       />
-      <button className="remove" onClick={props.remove}>
-        X
-      </button>
-      <h2>{props.name}</h2>
-      <div className="likes_container">
-        <p className="likes">❤ {props.like}</p>
-        <button onClick={props.add}> Add like </button>
-      </div>
+      <button className="remove" onClick={props.remove}>X</button>
+        <div className="cardFooter">
+            <h2 className="title">{props.name}</h2>
+            <div className="likes_container">
+                <p className="likes">❤ {props.like}</p>
+                <button onClick={props.add}> like </button>
+            </div>
+        </div>
     </div>
   );
 };
